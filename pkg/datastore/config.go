3493bb1e9e3a19ab4b61b447d7c16d5426836bdc
Danny <dannyvankooten@gmail.com>
2018-05-08 16:21:22 +0200

    default to actual driver name instead of alias

--- pkg/datastore/config.go
+++ pkg/datastore/config.go
@@ -3,7 +3,7 @@ package datastore
 import "fmt"
 
 type Config struct {
-	Driver   string `default:"sqlite"`
+	Driver   string `default:"sqlite3"`
 	Host     string `default:""`
 	User     string `default:""`
 	Password string `default:""`
@@ -18,9 +18,8 @@ func (c *Config) DSN() string {
 		dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s", c.Host, c.User, c.Password, c.Name)
 	case "mysql":
 		dsn = fmt.Sprintf("%s:%s@%s/%s?parseTime=true&loc=Local", c.User, c.Password, c.Host, c.Name)
-	case "sqlite3", "sqlite":
-
-		dsn = c.Name + "?_loc=auto" // TODO: Make this configurable
+	case "sqlite3":
+		dsn = c.Name + "?_loc=auto"
 	}
 
 	return dsn
