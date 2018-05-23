fb36c6ae6cb195620a0c5e61773f9702fb550334
Danny <dannyvankooten@gmail.com>
2018-05-23 13:44:35 +0200

    don't exit install script on err

--- install.sh
+++ install.sh
@@ -4,12 +4,11 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 # 
 # Defaults
-set -e               # Exit on error
 SERVER_PORT="9000"   # Port for Fathom server
 
 # Check if running as root
-if [ "$EUID" -ne 0 ]
-  then echo "Please run the Fathom installer as root (to install the necessary NGINX & systemd files)"
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run the Fathom installer as root (to install the necessary NGINX & systemd files)"
   exit
 fi
 
