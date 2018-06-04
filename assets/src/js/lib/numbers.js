c2e63a2dd3b62a8666a476d0c4bbdb7cb2665343
Danny <dannyvankooten@gmail.com>
2018-06-04 14:03:50 +0200

    improve number format perf

--- assets/src/js/lib/numbers.js
+++ assets/src/js/lib/numbers.js
@@ -1,14 +1,23 @@
 'use strict';
 
+const M = 1000000
+const K = 1000
+const rx = new RegExp('\\.0$');
+const commaRx = new RegExp('(\\d+)(\\d{3})');
+
 function formatPretty(num) {
-  let M = 1000000, K = 1000;
+  let decimals = 0;
 
   if (num >= M) {
-    return (num / M).toFixed(num > (M * 10) ? 1 : 2).replace(/\.0$/, '') + 'M';
+    num /= M
+    decimals = 3 - ((Math.round(num) + "").length) || 0;
+    return num.toFixed(decimals > -1 ? decimals : 0).replace(rx, '') + 'M';
   }
 
   if (num >= (K * 10)) {
-    return (num / K).toFixed(num > (K*100) ? 0 : 1).replace(/\.0$/, '') + 'K';
+    num /= K
+    decimals = 3 - ((Math.round(num) + "").length) || 0;
+    return num.toFixed(decimals).replace(rx, '') + 'K';
   }
 
   return formatWithComma(num);
@@ -24,9 +33,8 @@ function formatWithComma(nStr) {
   var	x = nStr.split('.');
 	var x1 = x[0];
 	var x2 = x.length > 1 ? '.' + x[1] : '';
-	var rgx = /(\d+)(\d{3})/;
-	while (rgx.test(x1)) {
-		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	while (commaRx.test(x1)) {
+		x1 = x1.replace(commaRx, '$1' + ',' + '$2');
 	}
 	return x1 + x2;
 }
