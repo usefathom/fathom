5507fbcd9226738878becb22ddf0b8b3dbbd6dc1
Danny van Kooten <dannyvankooten@gmail.com>
2018-09-21 10:50:34 +0200

    update test for respond func

--- pkg/api/http_test.go
+++ pkg/api/http_test.go
@@ -2,17 +2,22 @@ package api
 
 import (
 	"encoding/json"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestRespond(t *testing.T) {
 	w := httptest.NewRecorder()
-	respond(w, 15)
+	respond(w, http.StatusOK, 15)
+
+	if w.Code != 200 {
+		t.Errorf("Invalid response code")
+	}
 
 	// assert json header
 	if w.Header().Get("Content-Type") != "application/json" {
-		t.Errorf("Invalid Content-Type header")
+		t.Errorf("Invalid response header for Content-Type")
 	}
 
 	// assert json response
