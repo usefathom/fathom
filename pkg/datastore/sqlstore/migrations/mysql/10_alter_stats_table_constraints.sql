c877319046faec6a64c53b0643143cef1a23697d
Danny van Kooten <dannyvankooten@gmail.com>
2018-10-10 10:27:16 +0200

    fix DROP INDEX statements for mysql

--- pkg/datastore/sqlstore/migrations/mysql/10_alter_stats_table_constraints.sql
+++ pkg/datastore/sqlstore/migrations/mysql/10_alter_stats_table_constraints.sql
@@ -1,8 +1,8 @@
 -- +migrate Up
 
-DROP INDEX IF EXISTS unique_daily_site_stats;
-DROP INDEX IF EXISTS unique_daily_page_stats;
-DROP INDEX IF EXISTS unique_daily_referrer_stats;
+DROP INDEX unique_daily_site_stats ON daily_site_stats; 
+DROP INDEX unique_daily_page_stats ON daily_page_stats;
+DROP INDEX unique_daily_referrer_stats ON daily_referrer_stats;
 
 CREATE UNIQUE INDEX unique_daily_site_stats ON daily_site_stats(site_id, date);
 CREATE UNIQUE INDEX unique_daily_page_stats ON daily_page_stats(site_id, hostname, pathname, date);
@@ -10,9 +10,9 @@ CREATE UNIQUE INDEX unique_daily_referrer_stats ON daily_referrer_stats(site_id,
 
 -- +migrate Down
 
-DROP INDEX IF EXISTS unique_daily_site_stats;
-DROP INDEX IF EXISTS unique_daily_page_stats;
-DROP INDEX IF EXISTS unique_daily_referrer_stats;
+DROP INDEX unique_daily_site_stats ON daily_site_stats; 
+DROP INDEX unique_daily_page_stats ON daily_page_stats;
+DROP INDEX unique_daily_referrer_stats ON daily_referrer_stats;
 
 CREATE UNIQUE INDEX unique_daily_site_stats ON daily_site_stats(date);
 CREATE UNIQUE INDEX unique_daily_page_stats ON daily_page_stats(hostname, pathname, date);
