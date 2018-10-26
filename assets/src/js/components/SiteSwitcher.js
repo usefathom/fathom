036c36933360f3b1e5716c48b996bdcfa3e629cb
Samuel Meuli <me@samuelmeuli.com>
2018-10-26 15:42:06 +0200

    make SiteSwitcher work on touch devices (#164)

--- assets/src/js/components/SiteSwitcher.js
+++ assets/src/js/components/SiteSwitcher.js
@@ -4,6 +4,13 @@ import { h, Component } from 'preact';
 import { bind } from 'decko';
 
 class SiteSwitcher extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isExpanded: false
+    };
+  }
+
   @bind 
   selectSite(evt) {
     let itemId = evt.target.getAttribute("data-id")  
@@ -22,6 +29,27 @@ class SiteSwitcher extends Component {
       this.props.onAdd({ id: 1, name: "New site", unsaved: true })
   }
 
+  @bind
+  expand() {
+    this.setState({
+      isExpanded: true
+    });
+  }
+
+  @bind
+  collapse() {
+    this.setState({
+      isExpanded: false
+    });
+  }
+
+  @bind
+  toggleExpanded() {
+    this.setState({
+      isExpanded: !this.state.isExpanded
+    });
+  }
+
   render(props, state) {
     // show nothing if there is only 1 site and no option to add additional sites
     if(!props.showAdd && props.sites.length == 1) {
@@ -30,7 +58,7 @@ class SiteSwitcher extends Component {
 
     // otherwise, render list of sites + add button
     return (
-        <li class="sites">
+        <li class={`sites ${state.isExpanded ? 'expanded' : ''}`} onClick={this.toggleExpanded} onMouseEnter={this.expand} onMouseLeave={this.collapse}>
             <a href="javascript:void(0)">{props.selectedSite.name}</a>
             <ul>
                 {props.sites.map((s) => (<li class="site-switch"><a href="javascript:void(0);" data-id={s.id} onClick={this.selectSite}>{s.name}</a></li>)) }
